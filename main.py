c188577be17c71f1c12f4a1d43c7d1621173eed3
Prytkovkach <odprytkov1@gmail.com>
2023-04-06 18:49:54 +0300

    Added comments parsing

--- main.py
+++ main.py
@@ -56,16 +56,19 @@ def webpage_parsing(response):
     soup = BeautifulSoup(response.text, "lxml")
     title_tag = soup.find("h1")
     image_tag = soup.find("div", class_="bookimage").find("img")["src"]
+    comments_tag_list = soup.find_all("div", class_="texts")
+    comments_list = [comment_tag.find("span").text for comment_tag in comments_tag_list]
 
     image_extension = os.path.splitext(image_tag)
     image_url = urljoin(TULULU_BASE_URL, image_tag)
+
     title_text = title_tag.text
     splitted_title = title_text.strip().split("::")
 
     title = splitted_title[0].strip()
     author = splitted_title[1].strip()
 
-    return title, author, image_url, image_extension[1]
+    return title, author, image_url, image_extension[1], comments_list
 
 
 if __name__ == "__main__":
@@ -76,7 +79,7 @@ if __name__ == "__main__":
         response.raise_for_status()
         try:
             check_for_redirect(response)
-            title, author, image_url, image_extension = webpage_parsing(response)
+            title, author, image_url, image_extension, comments_list = webpage_parsing(response)
             download_txt(book_id, f"{book_id}. {title}")
             download_image(image_url, f"{book_id}{image_extension}")
         except ErrRedirection:
